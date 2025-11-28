151aeb08dc93cf2513f392eed5e4f56a5b523ee9
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2025-11-29 03:18:06 +0900

    카드 데이터 주석 작성

--- Assets/Scripts/Ingame/Datas/CardData.cs
+++ Assets/Scripts/Ingame/Datas/CardData.cs
@@ -2,6 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
+//카드의 외형 데이터 전달용 구조체
 [Serializable]
 public struct CardData
 {
@@ -10,8 +11,8 @@ public struct CardData
         #warning 카드 희귀도 목록 작성해야함
     }
 
-    public Sprite sprite;
-    public string name;
-    public string description;
-    public CardRarity rarity;
+    public Sprite sprite;     //카드 일러스트
+    public string name;     //카드 이름
+    public string description;     //카드 설명
+    public CardRarity rarity;     //카드 희귀도
 }
