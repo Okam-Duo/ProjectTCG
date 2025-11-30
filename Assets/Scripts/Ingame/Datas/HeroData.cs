4687b2e6fe2a41cc83d95b1d5293404960a33ad0
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2025-11-30 19:27:32 +0900

    영웅 데이터 주석 수정

--- Assets/Scripts/Ingame/Datas/HeroData.cs
+++ Assets/Scripts/Ingame/Datas/HeroData.cs
@@ -1,14 +1,12 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
 
-//카드의 외형 데이터 저장용 스크립터블 오브젝트
+//영웅의 외형 데이터 저장용 스크립터블 오브젝트
 //데이터베이스 적용 전 임시로 사용
 [CreateAssetMenu(fileName = "NewHeroData", menuName = "Custom/영웅카드 데이터", order = int.MinValue)]
 public class HeroData : ScriptableObject
 {
-    public Sprite Sprite;     //카드 일러스트
-    public string Name;     //카드 이름
+    public Sprite Sprite;     //영웅 일러스트
+    public string Name;     //영웅 이름
     public int MaxHealth => 10;    //최대 체력
 
     //스킬, 최대체력 등 데이터 추가할거면 해야됨
