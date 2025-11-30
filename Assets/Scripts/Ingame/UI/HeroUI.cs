e459793346434a4182cd55ea8603333f93bffd00
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2025-11-30 19:18:10 +0900

    이미지, 최대체력 반영하는 영웅 UI 작성

---
+++ Assets/Scripts/Ingame/UI/HeroUI.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroUI : MonoBehaviour
+{
+    [Header("컴포넌트 참조")]
+    [SerializeField] private Image _image;
+    [SerializeField] private TextMeshProUGUI _healthText;
+
+
+    private Vector3 _originPosition;
+
+
+    public void SetData(HeroData heroData)
+    {
+        _image.sprite = heroData.Sprite;
+        _healthText.text = heroData.MaxHealth.ToString();
+    }
+
+    public void SetOriginPosition(Vector3 position)
+    {
+        _originPosition = position;
+    }
+
+    public void SetHealth(int newHealth, bool enableAnimationByDamageValue = false)
+    {
+        _healthText.text = newHealth.ToString();
+
+        if (newHealth <= 0)
+        {
+            HandleDieAnimation();
+        }
+    }
+
+    public void HandleDieAnimation()
+    {
+        gameObject.SetActive(false);
+    }
+}
