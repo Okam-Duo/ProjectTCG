b8459541fcccbe56c61a73e51d67f6cc12fb3cef
chwoo8388 <chwoo8388@gmail.com>
2025-12-22 23:31:09 +0900

    shop 수정 2차

---
+++ Assets/Scripts/OutGame/CardPack_Information_UI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CardPack_Information_UI : MonoBehaviour
+{
+    public void ShowCardPackInformation()
+    {
+        gameObject.SetActive(true);
+
+    }
+
+    public int a;
+
+}
