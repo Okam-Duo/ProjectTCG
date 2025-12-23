1cfe0b6a44bbe5b91d2cf82e7a4a8b5377cde22b
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2025-12-24 04:05:40 +0900

    StaticDataManager에 주석 추가

--- Assets/Scripts/Ingame/StaticDataManager.cs
+++ Assets/Scripts/Ingame/StaticDataManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
-//id기반 정적 데이터 관리자
+/// <summary>
+/// ScriptableObject 데이터 id기반 참조용 매니저
+/// </summary>
 public sealed class StaticDataManager
 {
 
@@ -32,6 +34,9 @@ public sealed class StaticDataManager
 
     #endregion
 
+    /// <summary>
+    /// 데이터를 불러올 준비가 되었는지 확인합니다.
+    /// </summary>
     public static bool IsReady { get; private set; } = false;
 
     private StaticDataManager(Action onLoaded)
@@ -43,28 +48,31 @@ public sealed class StaticDataManager
         _onLoaded = onLoaded;
     }
 
-    public static void LoadTableAsync(Action onLoadedTable)
+    /// <summary>
+    /// 데이터를 비동기로 불러옵니다, 데이터가 불러와진 뒤 부터 GetData류 함수를 사용할 수 있습니다.
+    /// </summary>
+    public static void LoadTableAsync(Action onLoaded)
     {
-        _instance = new(onLoadedTable);
+        _instance = new(onLoaded);
     }
 
     public static CardData GetCardData(int id)
     {
-        Debug.Assert(IsReady, "테이블이 준비되지 않았습니다.");
+        Debug.Assert(IsReady, "테이블이 준비되지 않았습니다, LoadTableAsync함수를 호출하고 기다려주세요.");
 
         return _instance._cardDataLoader.GetData(id);
     }
 
     public static HeroData GetHeroData(int id)
     {
-        Debug.Assert(IsReady, "테이블이 준비되지 않았습니다.");
+        Debug.Assert(IsReady, "테이블이 준비되지 않았습니다, LoadTableAsync함수를 호출하고 기다려주세요.");
 
         return _instance._heroDataLoader.GetData(id);
     }
 
     public static PackData GetPackData(int id)
     {
-        Debug.Assert(IsReady, "테이블이 준비되지 않았습니다.");
+        Debug.Assert(IsReady, "테이블이 준비되지 않았습니다, LoadTableAsync함수를 호출하고 기다려주세요.");
 
         return _instance._packDataLoader.GetData(id);
     }
@@ -82,6 +90,7 @@ public sealed class StaticDataManager
         }
     }
 
+    //데이터 종류에 따라 불러오는 로직 일반화
     private class StaticDataLoader<DataT, TableT, HolderT>
         where TableT : IStaticDataTable<HolderT>
         where HolderT : IStaticDataHolder<DataT>
@@ -113,13 +122,25 @@ public sealed class StaticDataManager
 
     #endregion
 
+    #region 데이터 홀더, 테이블이 상속하는 인터페이스
+
+    /// <summary>
+    /// 데이터 홀더들을 배열로 저장해 ID를 배정하는 ScriptableObject
+    /// </summary>
+    /// <typeparam name="DataHolderT">테이블이 참조할 데이터 홀더</typeparam>
     public interface IStaticDataTable<DataHolderT>
     {
         DataHolderT[] Holders { get; }
     }
 
+    /// <summary>
+    /// 데이터를 입력하기 위한 ScriptableObject
+    /// </summary>
+    /// <typeparam name="DataT">홀더가 만들어낼 데이터 반환형</typeparam>
     public interface IStaticDataHolder<DataT>
     {
         DataT GetData();
     }
+
+    #endregion
 }
