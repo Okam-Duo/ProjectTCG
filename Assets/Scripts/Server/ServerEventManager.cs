e4b75cef209e195a2d9ee73601f51bbf1d1edacd
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2025-12-31 05:08:40 +0900

    패킷 수신 콜백 등록 양식 변경

--- Assets/Scripts/Server/ServerEventManager.cs
+++ Assets/Scripts/Server/ServerEventManager.cs
@@ -1,6 +1,8 @@
 using Shared.Network;
 using System;
 using System.Collections.Generic;
+using Shared;
+using System.Diagnostics;
 
 #warning 클라이언트 개발용 임시 어댑터 클래스, 추후 기능 연결 필요
 //추가 데이터는 전부 long타입으로 때워놨으니 나중에 변경하거나 로직 추가 필요
@@ -13,12 +15,35 @@ public class ServerEventManager
     //싱글턴
     public static ServerEventManager Instance { get; private set; } = new ServerEventManager();
 
-    public Dictionary<Type, Action<IPacket>> OnRecievePacket { get; private set; }
+    //콜백은 메인스레드에서 작동함이 보장됨
+    private Dictionary<PacketID, Action<IPacket>> _onRecievePacket = new();
 
     private ServerEventManager() { }
 
     public void Send(IPacket packet)
     {
         //TODO
+    }
+
+    public void RegistCallback(PacketID id, Action<IPacket> callback)
+    {
+        if (_onRecievePacket.ContainsKey(id))
+        {
+            _onRecievePacket[id] += callback;
+        }
+        else
+        {
+            _onRecievePacket[id] = callback;
+        }
+    }
+
+    public void UnregistCallback(PacketID id, Action<IPacket> callback)
+    {
+        Debug.Assert(_onRecievePacket.ContainsKey(id), "콜백이 없는 패킷ID의 콜백을 등록해제하려고 시도했습니다.");
+
+        if (_onRecievePacket.ContainsKey(id))
+        {
+            _onRecievePacket[id] -= callback;
+        }
     }
 }
