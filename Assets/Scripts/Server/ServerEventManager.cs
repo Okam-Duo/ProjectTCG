93de8e790f006ad568977c6537f57c0ba9bd6d54
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2025-12-31 05:10:50 +0900

    ServerEventManager 주석 수정

--- Assets/Scripts/Server/ServerEventManager.cs
+++ Assets/Scripts/Server/ServerEventManager.cs
@@ -5,9 +5,6 @@ using Shared;
 using System.Diagnostics;
 
 #warning 클라이언트 개발용 임시 어댑터 클래스, 추후 기능 연결 필요
-//추가 데이터는 전부 long타입으로 때워놨으니 나중에 변경하거나 로직 추가 필요
-//커플링을 신경쓸 필요가 없다고 판단될 시 패킷 수신 이벤트를 콜백에서 함수로 변경 바람
-
 
 //패킷 송수신 어댑터
 public class ServerEventManager
